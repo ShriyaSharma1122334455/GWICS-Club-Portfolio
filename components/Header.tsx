cfe077615ea400f195c0e38caf4047b557c7a4a9
Shri <ss5379@njit.edu>
2025-09-16 18:29:51 -0400

    adding a logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -55,7 +55,11 @@ const Header: React.FC<HeaderProps> = ({ activeSection }) => {
           <div className="flex items-center justify-between">
             <div className="flex items-center space-x-4">
               <a href="#home" className="flex items-center space-x-2">
-                <GwicsLogo className="h-16 w-16" />
+                <img
+                  src="public/gwics.png"
+                  alt="GWICS Logo"
+                  className="h-12 w-12"
+                />
                 <span className="text-slate-800 font-semibold hidden sm:block">
                   Graduate Women in Computing Society
                 </span>
--- components/icons/GwicsLogo.tsx
+++ components/icons/GwicsLogo.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const GwicsLogo: React.FC<{ className?: string }> = ({ className }) => (
-  <img src="/gwics.png" alt="GWICS Logo" className={className || "h-12 w-12"} />
+  <img
+    src="public/gwics.png"
+    alt="GWICS Logo"
+    className={className || "h-12 w-12"}
+  />
 );
 
 export default GwicsLogo;
