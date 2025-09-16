75c6a9d25eff8df1e592fa764708b7ea77ae29c7
Shri <shriyasharma2152@gmail.com>
2025-09-16 19:10:11 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,12 +9,12 @@ const Footer: React.FC = () => {
           <div className="flex flex-col space-y-4">
             <div className="flex items-center space-x-4">
               <div className="bg-white rounded-full p-1">
-                <img src="../public/gwics.png" alt="GWICS Logo" className="h-14 w-14" />
+                <img src="../gwics.png" alt="GWICS Logo" className="h-12 w-12" />
               </div>
               <img
                 src="../public/nit.png"
                 alt="NJIT Logo"
-                className="h-10 bg-white p-1 rounded"
+                className="h-12 bg-white p-1 rounded"
               />
             </div>
             <p className="text-slate-400 text-sm">
